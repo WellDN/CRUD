b45df3c3f8e21583e61b10a72a636e7588c24da0
WellDN <Wellingtontp0@gmail.com>
2022-11-20 14:06:44 -0300

    auth login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-
-import './App.css'
-
 function App() {
-  
+    return(
+        <body>
+        <div>
+        <h1 className="font-bold">
+            Login
+         <textarea className="h-6 resize rounded-md">
+            </textarea>
+            </h1>
+        <h1 className="">
+            password 
+            <textarea className="h-6 resize rounded-md">
+                </textarea>
+                </h1>
+        </div>
+        <div className="flex justify-center">
+            <button className="">Auth</button>
+        </div>
+        </body>
+    )
 }
 
 export default App
