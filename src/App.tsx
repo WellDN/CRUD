1128d11af2ff3f4f88a39986f18a69bf052cbfd9
WellDN <Wellingtontp0@gmail.com>
2022-11-24 20:26:57 -0300

    :octocat:

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
             <Route path="/" element={<Home />} />
             
         </Routes>
+        
     )
 }
 
--- src/auth.tsx
+++ src/auth.tsx
@@ -1,56 +1,14 @@
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
 
-type WrapProps = {
-  valid: JSX.Element | boolean
-  profileObj: boolean
-}
+<GoogleOAuthProvider 
+    clientId="<828311702305-3o4usi5raebpt6j1lim7rip91in43b0m.apps.googleusercontent.com>">
 
-const userId = "828311702305-3o4usi5raebpt6j1lim7rip91in43b0m.apps.googleusercontent.com"
-
-function IAuth (res: WrapProps) {
-
-const valid = () => {
-  console.log('Valid', res.profileObj);
-}
-
-const notValid = () => {
-  console.log('F', res)
-}
-
-function Logout () {
-
-  const onSucess = () => {
-    console.log('Log out w');
-  }
-
-  return (
-    <div>{onSucess}</div>
-  )
-}
-
-}
-
-
-function Auth () {
-
-useEffect(() => {
-  function start() {
-    gapi.client.init({
-      clientId: userId,
-      scope: ""
-    })
-  };
-
-  gapi.load('client:auth2', start)
-})
-
-return (
-  <div className="flex justify-center">
-  <button className="">Auth
-    {valid} {notValid} {logout}
-  </button>
-</div>
-)
-}
+<GoogleLogin
+  onSuccess={credentialResponse => {
+    return (credentialResponse);
+  }}
+  onError={() => {
+    return(<h1>login Failed</h1>)
+  }}
+/>
+  </GoogleOAuthProvider>
