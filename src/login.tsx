f47c0fb656d717ca97bcff0eba0bebbee8ef7ce3
WellDN <Wellingtontp0@gmail.com>
2022-12-02 18:56:02 -0300

    :octocat:

--- src/login.tsx
+++ src/login.tsx
@@ -1,64 +1,69 @@
 import { ReactElement, useEffect, useState } from 'react';
 
-export function Login(): ReactElement {
-    
-    
-        const [password, setPassword] = useState({
-          firstPassword: '',
-          confirmPassword: ''
-        })
-        
-        const [validLength, setValidLength] = useState(false);
-        const [hasNumber, setHasNumber] = useState(false);
-        const [upperCase, setUpperCase] = useState(false);
-        const [lowerCase, setLowerCase] = useState(false);
-        const [specialChar, setSpecialChar] = useState(false);
-        const [match, setMatch] = useState(false);
-        const [requiredLength, setRequiredLength] = useState(8)
-      
+type User = {
+  name: string;
+  email: string;
+  password: string;
+}
 
-    const inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (event) => {
-        const { value, name } = event.target;
-        setPassword({
-          ...password,
-          [name]: value
-        })
-      }
-      useEffect(() => {
-        setValidLength(password.firstPassword.length >= requiredLength ? true : false);
-        setUpperCase(password.firstPassword.toLowerCase() !== password.firstPassword);
-        setLowerCase(password.firstPassword.toUpperCase() !== password.firstPassword);
-        setHasNumber(/\d/.test(password.firstPassword));
-        setMatch(!!password.firstPassword && password.firstPassword === password.confirmPassword)
-        setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(password.firstPassword));
-    
-      }, [password, requiredLength]);
-    
-      return (
-        <>
-        <div>
-        <div className="w-full max-w-xs">
+export function Login(): ReactElement {
+
+  const [password, setPassword] = useState({
+    firstPassword: '',
+    confirmPassword: ''
+  })
+  const [register, isRegistered] = useState(false);
+  const [login, isLogged] = useState();
+  
+  const [validLength, setValidLength] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [upperCase, setUpperCase] = useState(false);
+  const [lowerCase, setLowerCase] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const [match, setMatch] = useState(false);
+  const [requiredLength, setRequiredLength] = useState(8);
+
+const inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (event) => {
+    const { value, name } = event.target;
+    setPassword({
+      ...password,
+      [name]: value
+    })
+  }
+  useEffect(() => {
+    setValidLength(password.firstPassword.length >= requiredLength ? true : false);
+    setUpperCase(password.firstPassword.toLowerCase() !== password.firstPassword);
+    setLowerCase(password.firstPassword.toUpperCase() !== password.firstPassword);
+    setHasNumber(/\d/.test(password.firstPassword));
+    setMatch(!!password.firstPassword && password.firstPassword === password.confirmPassword)
+    setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(password.firstPassword));
+
+  }, [password, requiredLength]);
+
+  return (
+    <>
+    <div>
+    <div className="w-full max-w-xs">
   <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
+  <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
         Username
-        <input onChange={inputChange} name="firstPassword" type='text' />
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="Username">
-      </input>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
     </div>
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstPassword">
-        First Password
+      Password
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      <input onChange={inputChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" name="firstPassword" type="password" placeholder="******************">
       </input>
     </div>
     <div className="mb-6">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
         Confirm Password
       </label>
-      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
+      <input onChange={inputChange} className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="confirmPassword" type="password" placeholder="******************">
+      </input>
       <p className="text-red-500 text-xs italic">Please confirm your password.</p>
     </div>
     <div className="flex items-center justify-between">
@@ -72,7 +77,6 @@ export function Login(): ReactElement {
   </form>
   <p className="text-center text-gray-500 text-xs">
   </p>
-</div>
 </div>
 <ul>
             <li>
@@ -91,9 +95,9 @@ export function Login(): ReactElement {
             <li>
               Special Character: {specialChar ? <span>True</span> : <span>False</span>}
             </li>
-          </ul>
-      
-    
+          </ul>    
+</div>
+
 
         <div className="w-full max-w-xs">
   <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -122,7 +126,6 @@ export function Login(): ReactElement {
   <p className="text-center text-gray-500 text-xs">
   </p>
 </div>
-
 </>
     )
 }
