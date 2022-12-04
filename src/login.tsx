81eb6d9c17d68ccb6d11c012c7d76736410ea0d6
WellDN <Wellingtontp0@gmail.com>
2022-12-04 19:22:59 -0300

    :octocat:

--- src/login.tsx
+++ src/login.tsx
@@ -1,4 +1,14 @@
 import { ReactElement, useEffect, useState } from 'react';
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import { Route } from 'react-router-dom';
+import multer from 'multer'
+import error from 'http-errors';
+import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 type User = {
   name: string;
@@ -8,6 +18,165 @@ type User = {
 
 
 export function Login(): ReactElement {
+
+  const serv = express();
+
+serv.use(bodyParser.urlencoded({ extended: true}))
+serv.use(bodyParser.json());
+
+serv.use('/', Route);
+
+  const port = process.env.PORT || 5000;
+  serv.listen(port, () => {
+    return(`server is running on port ${port}`);
+  })
+
+
+  const router = express.Router();
+  
+  router.get('/', (_req, res) => {
+    res.send('Hello World');
+  })
+  router.use('/auth', );
+  router.use(async (_req, _res, next ) => {
+    next(createHttpError.NotFound('Route Not Found'))
+  })    //@ts-ignore
+  router.use( (err, _req, res, _next) => {
+    res.status(err.status || 500).json({
+        status: false,
+        message: err.message
+    })
+})
+module.exports = router
+
+const accessTokenSecret: any = process.env.ACCESS_TOKEN_SECRET
+module.exports = {
+    signAccessToken(payload:boolean){
+        return new Promise((resolve, reject) => {
+            jwt.sign({ payload }, accessTokenSecret, {
+            }, (err, token) => {
+                if (err) {
+                reject(createHttpError.InternalServerError())
+                }
+                resolve(token)
+            })
+        })
+    },
+    verifyAccessToken(token: string){
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, accessTokenSecret, (err: string | any, payload: string | any) => {
+                if (err) {
+                    const message = err.name == 'JsonWebTokenError' ? 'Unauthorized' : err.message
+                    return reject(createHttpError.Unauthorized(message))
+                }
+                resolve(payload)
+            })
+        })
+    }
+}
+
+const prisma = new PrismaClient();
+
+class AuthService {
+  static async register(data:string | any) {
+        const { email:string } = data;
+        data.password = bcrypt.hashSync(data.password, 8);
+        let user = prisma.user.create({
+            data
+        })//@ts-ignore
+        data.accessToken = await jwt.signAccessToken(user);
+
+        return data;
+    }
+}
+
+module.exports = AuthService;
+
+class lmao {
+  static async iLogin(data: User) {
+      const { email, password } = data;
+      const user = await prisma.user.findUnique({
+          where: {
+              email
+          }
+      });
+      if (!user) {
+          throw createHttpError.NotFound('User not registered')
+      }//@ts-ignore
+      const checkPassword = bcrypt.compareSync(password, user.password)
+      if (!checkPassword) throw createHttpError.Unauthorized('Email address or password not valid')
+        //@ts-ignore
+      delete user.password
+      //@ts-ignore
+      const accessToken = await jwt.signAccessToken(user)
+      return { ...user, accessToken }
+    }
+    static async all() {
+      const allUsers = await prisma.user.findMany();
+      return allUsers;
+    }
+  }
+
+  static register = async (req, res, next) => {
+    try {
+        const user = await auth.register(req.body);
+        res.status(200).json({
+            status: true,
+            message: 'User created successfully',
+            data: user
+        })
+    }
+    catch (e) {
+        next(createError(e.statusCode, e.message))
+    }
+}
+static login = async (req, res, next) => {
+     try {
+        const data = await auth.login(req.body)
+        res.status(200).json({
+            status: true,
+            message: "Account login successful",
+            data
+        })
+    } catch (e) {
+        next(createError(e.statusCode, e.message))
+    }
+}
+static all = async (req, res, next) => {
+    try {
+        const users = await auth.all();
+        res.status(200).json({
+            status: true,
+            message: 'All users',
+            data: users
+        })
+    }
+    catch (e) {
+        next(createError(e.statusCode, e.message))
+    }
+}
+}
+
+module.exports = authController;
+
+const auth = async (req, res, next) => {
+  if (!req.headers.authorization) {
+      return next(createError.Unauthorized('Access token is required'))
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  if (!token) {
+      return next(createError.Unauthorized())
+  }
+  await jwt.verifyAccessToken(token).then(user => {
+      req.user = user
+      next()
+  }).catch (e => {
+      next(createError.Unauthorized(e.message))
+  })
+}
+
+module.exports = auth;
+
 
 
   const [password, setPassword] = useState({
