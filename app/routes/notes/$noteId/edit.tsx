12c51c62956835ad0d067e12735994dc3022fd8d
WellDN <Wellingtontp0@gmail.com>
2023-02-15 17:51:46 -0300

    - edit, Dynamic segments

--- app/routes/notes/$noteId/edit.tsx
+++ app/routes/notes/$noteId/edit.tsx
@@ -1,6 +1,7 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node"
 import { redirect, json } from "@remix-run/node"
-import { Form, Link, useLoaderData } from "@remix-run/react"
+import { Form, useActionData, useLoaderData } from "@remix-run/react"
+import React from "react";
 import invariant from "tiny-invariant";
 import { editNote, getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server"
@@ -39,33 +40,88 @@ if (typeof title !== "string" || title.length === 0) {
 
     await editNote({ userId, id: params.noteId, body, title });
     
-    return redirect(`/notes/`)
+    return redirect(`/notes/${params.id}`)
 }
 
 export default function EditNotes() {
     const data = useLoaderData<typeof loader>();
-    
-    return(
-        (
-    <div className="">
-    <div className="justify-end flex">
-    <Link to="./edit"
+
+    const actionData = useActionData<typeof action>();
+    const titleRef = React.useRef<HTMLInputElement>(null);
+    const bodyRef = React.useRef<HTMLTextAreaElement>(null);
+
+    React.useEffect(() => {
+            if (actionData?.errors?.title) {
+            titleRef.current?.focus();
+            } else if (actionData?.errors?.body) {
+            bodyRef.current?.focus();
+            }
+            }, [actionData]);
+
+  return (
+    <Form
+      method="post"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 8,
+        width: "100%",
+      }}
     >
-    📝 edit
-    </Link>
-    </div>
-      <h3 className="text-2xl font-bold">{data.note.title}</h3>
-      <p className="py-6">{data.note.body}</p>
-      <hr className="my-4" />
-      <Form method="post">
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>
+          Title:
+          </span>
+          <input
+            defaultValue={data.note.title}
+            ref={titleRef}
+            name="title"
+            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            aria-invalid={actionData?.errors?.title ? true : undefined}
+            aria-errormessage={
+              actionData?.errors?.title ? "title-error" : undefined
+            }
+          />
+        </label>
+        {actionData?.errors?.title && (
+          <div className="pt-1 text-red-700" id="title-error">
+            {actionData.errors.title}
+          </div>
+        )}
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>
+          Body: 
+          </span>
+          <textarea
+            defaultValue={data.note.body}
+            ref={bodyRef}
+            name="body"
+            rows={8}
+            className="w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-6"
+            aria-invalid={actionData?.errors?.body ? true : undefined}
+            aria-errormessage={
+              actionData?.errors?.body ? "body-error" : undefined
+            }
+          />
+        </label>
+        {actionData?.errors?.body && (
+          <div className="pt-1 text-red-700" id="body-error">
+            {actionData.errors.body}
+          </div>
+        )}
+      </div>
+      <div className="text-right">
         <button
           type="submit"
           className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
         >
-        Save
+          Save
         </button>
+      </div>
       </Form>
-    </div>
   )
-    )
 }
